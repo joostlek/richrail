6c612f60abe9d4ee8ac067cf67118b9f2ff6749c
joostlek <joostlek@outlook.com>
2018-12-14 16:19:59 +0100

    Update Database config
    Signed-off-by: joostlek <joostlek@outlook.com>

--- src/main/java/nl/hu/richrail/persistence/DatabaseConfig.java
+++ src/main/java/nl/hu/richrail/persistence/DatabaseConfig.java
@@ -1,11 +1,30 @@
 package nl.hu.richrail.persistence;
 
+import nl.hu.richrail.exceptions.NoPropertiesSetException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class DatabaseConfig {
 
-    public String host;
+    String host;
 
-    public String username;
+    String username;
 
-    public String password;
+    String password;
 
+    public DatabaseConfig() throws IOException {
+        String path = DatabaseConfig.class.getResource("/").getPath() + "app.properties";
+        Properties appProps = new Properties();
+        FileInputStream fileInputStream = new FileInputStream(path);
+        appProps.load(fileInputStream);
+        if (appProps.get("DB.HOST").equals("")) {
+            throw new NoPropertiesSetException();
+        }
+        this.host = appProps.getProperty("DB.HOST");
+        this.username = appProps.getProperty("DB.USER");
+        this.password = appProps.getProperty("DB.PASS");
+        fileInputStream.close();
+    }
 }
