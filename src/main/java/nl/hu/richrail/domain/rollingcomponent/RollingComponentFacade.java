70dcf303405e974bf17c9652149aa2ede9fa902a
Dennis <dennisvk@users.noreply.github.com>
2018-12-14 22:01:43 +0100

    Add new service to use TrainFacade and RollingComponentFacade

---
+++ src/main/java/nl/hu/richrail/domain/rollingcomponent/RollingComponentFacade.java
@@ -0,0 +1,93 @@
+package nl.hu.richrail.domain.rollingcomponent;
+
+import nl.hu.richrail.domain.rollingcomponent.type.LocomotiveBuilder;
+import nl.hu.richrail.domain.rollingcomponent.type.Wagon;
+import nl.hu.richrail.domain.rollingcomponent.type.WagonBuilder;
+import nl.hu.richrail.services.TrainService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RollingComponentFacade {
+    // TODO: implement DAO
+
+    private List<RollingComponent> rollingComponents = new ArrayList<>();
+
+    private static Logger logger = Logger.getLogger(TrainService.class.getName());
+
+    public RollingComponent getRollingComponent(String id) {
+        for (RollingComponent rollingComponent : rollingComponents) {
+            if (rollingComponent.getId().equalsIgnoreCase(id)) {
+                return rollingComponent.clone();
+            }
+        }
+
+        return null;
+    }
+
+    public List<RollingComponent> getRollingComponents() {
+        return rollingComponents;
+    }
+
+    private boolean createRollingComponent(RollingComponent rollingComponent) {
+        if (rollingComponents.contains(rollingComponent)) {
+            return false;
+        }
+
+        if (rollingComponents.add(rollingComponent)) {
+            logger.log(Level.INFO, "Created rollingcomponent: " + rollingComponent.toString());
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean createRollingComponentLocomotive(String id) {
+        String imagePath = "";
+
+        if (id == null) {
+            return false;
+        }
+
+        LocomotiveBuilder locomotiveBuilder = new LocomotiveBuilder(id, imagePath);
+
+        return createRollingComponent(locomotiveBuilder.getBuildResult());
+    }
+
+    public boolean createRollingComponentWagon(String id, int numSeats) {
+        String imagePath = "";
+
+        if (id == null) {
+            return false;
+        }
+
+        WagonBuilder wagonBuilder = new WagonBuilder(id, imagePath);
+
+        wagonBuilder.setNumSeats(numSeats);
+
+        return createRollingComponent(wagonBuilder.getBuildResult());
+    }
+
+    public boolean removeRollingComponent(String id) {
+        RollingComponent rollingComponent = getRollingComponent(id);
+
+        if (rollingComponents.remove(rollingComponent)) {
+            logger.log(Level.INFO, "Romoved rollingcomponent: " + rollingComponent.toString());
+            return true;
+        }
+
+        return false;
+    }
+
+    public String getRollingComponentsString() {
+        StringBuilder stringBuilder = new StringBuilder("rollingcomponents\n");
+
+        for (RollingComponent rollingComponent : rollingComponents) {
+            stringBuilder.append("(" + rollingComponent.getId() + (rollingComponent instanceof Wagon ? ": " + ((Wagon) rollingComponent).getNumSeats() : "") + ")\n");
+        }
+
+        return stringBuilder.toString();
+    }
+}
--- src/main/java/nl/hu/richrail/services/TrainService.java
+++ src/main/java/nl/hu/richrail/services/TrainService.java
@@ -1,25 +1,106 @@
 package nl.hu.richrail.services;
 
 import nl.hu.richrail.domain.Train;
+import nl.hu.richrail.domain.TrainFacade;
 import nl.hu.richrail.domain.rollingcomponent.RollingComponent;
+import nl.hu.richrail.domain.rollingcomponent.RollingComponentFacade;
 
 import java.util.List;
 
-public interface TrainService {
-    Train getTrain(String id);
-    List<Train> getTrains();
-    boolean createTrain(String id);
-    boolean removeTrain(String id);
-    int getTotalNumSeats(String id);
+public class TrainService implements TrainServiceInterface {
+    private static TrainService instance;
 
-    RollingComponent getRollingComponentFromTrain(String trainId, String rollingComponentId);
-    List<RollingComponent> getRollingComponentsFromTrain(String trainId);
-    boolean addRollingComponentToTrain(String trainId, RollingComponent rollingComponent);
-    boolean removeRollingComponentFromTrain(String trainId, RollingComponent rollingComponent);
+    TrainFacade trainFacade = new TrainFacade();
+    RollingComponentFacade rollingComponent = new RollingComponentFacade();
 
-    RollingComponent getRollingComponent(String id);
-    List<RollingComponent> getRollingComponents();
-    boolean createRollingComponentLocomotive(String id);
-    boolean createRollingComponentWagon(String id, int numSeats);
-    boolean removeRollingComponent(String id);
+    @Override
+    public Train getTrain(String id) {
+        return trainFacade.getTrain(id);
+    }
+
+    @Override
+    public List<Train> getTrains() {
+        return trainFacade.getTrains();
+    }
+
+    @Override
+    public boolean createTrain(String id) {
+        return trainFacade.createTrain(id);
+    }
+
+    @Override
+    public boolean removeTrain(String id) {
+        return trainFacade.removeTrain(id);
+    }
+
+    @Override
+    public int getTotalNumSeats(String id) {
+        return trainFacade.getTotalNumSeats(id);
+    }
+
+    @Override
+    public String getTrainsString() {
+        return trainFacade.getTrainsString();
+    }
+
+    @Override
+    public RollingComponent getRollingComponentFromTrain(String trainId, String rollingComponentId) {
+        return trainFacade.getRollingComponentFromTrain(trainId, rollingComponentId);
+    }
+
+    @Override
+    public List<RollingComponent> getRollingComponentsFromTrain(String trainId) {
+        return trainFacade.getRollingComponentsFromTrain(trainId);
+    }
+
+    @Override
+    public boolean addRollingComponentToTrain(String trainId, RollingComponent rollingComponent) {
+        return trainFacade.addRollingComponentToTrain(trainId, rollingComponent);
+    }
+
+    @Override
+    public boolean removeRollingComponentFromTrain(String trainId, RollingComponent rollingComponent) {
+        return trainFacade.removeRollingComponentFromTrain(trainId, rollingComponent);
+    }
+
+    @Override
+    public RollingComponent getRollingComponent(String id) {
+        return rollingComponent.getRollingComponent(id);
+    }
+
+    @Override
+    public List<RollingComponent> getRollingComponents() {
+        return rollingComponent.getRollingComponents();
+    }
+
+    @Override
+    public boolean createRollingComponentLocomotive(String id) {
+        return rollingComponent.createRollingComponentLocomotive(id);
+    }
+
+    @Override
+    public boolean createRollingComponentWagon(String id, int numSeats) {
+        return rollingComponent.createRollingComponentWagon(id, numSeats);
+    }
+
+    @Override
+    public boolean removeRollingComponent(String id) {
+        return rollingComponent.removeRollingComponent(id);
+    }
+
+    @Override
+    public String getRollingComponentsString() {
+        return rollingComponent.getRollingComponentsString();
+    }
+
+    /*
+     * Return trainfacade instance when DAO is not implemented
+     */
+    public static TrainService getInstance() {
+        if (instance == null) {
+            instance = new TrainService();
+        }
+
+        return instance;
+    }
 }
