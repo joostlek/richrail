9243646a49458d3b9ff269e8df27103c5314f502
Dennis <dennisvk@users.noreply.github.com>
2018-12-14 22:12:44 +0100

    Remove unused code in Main

--- src/main/java/nl/hu/richrail/Main.java
+++ src/main/java/nl/hu/richrail/Main.java
@@ -1,15 +1,10 @@
 package nl.hu.richrail;
 
 import nl.hu.richrail.application.ApplicationPicker;
-import nl.hu.richrail.services.TrainFacade;
 
 import javax.swing.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
-
-    private static Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new ApplicationPicker().createAndShow());
