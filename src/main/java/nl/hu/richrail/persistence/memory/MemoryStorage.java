3f8c9bf64799cc934b674791c26d732037e46ba7
Mike <aeonlucid@outlook.com>
2018-12-16 00:00:16 +0100

    Go.

--- src/main/java/nl/hu/richrail/persistence/memory/MemoryStorage.java
+++ src/main/java/nl/hu/richrail/persistence/memory/MemoryStorage.java
@@ -3,21 +3,28 @@ package nl.hu.richrail.persistence.memory;
 import nl.hu.richrail.persistence.ComponentRepository;
 import nl.hu.richrail.persistence.StorageMethod;
 import nl.hu.richrail.persistence.TrainRepository;
+import nl.hu.richrail.persistence.memory.repositories.MemoryComponentRepository;
+import nl.hu.richrail.persistence.memory.repositories.MemoryTrainRepository;
 
 public class MemoryStorage implements StorageMethod {
 
-    public MemoryStorage() {
+    private final TrainRepository trainRepository;
 
+    private final ComponentRepository componentRepository;
+
+    public MemoryStorage() {
+        this.trainRepository = new MemoryTrainRepository();
+        this.componentRepository = new MemoryComponentRepository();
     }
 
     @Override
     public TrainRepository getTrainRepository() {
-        return null;
+        return this.trainRepository;
     }
 
     @Override
     public ComponentRepository getComponentRepository() {
-        return null;
+        return this.componentRepository;
     }
 
 }
