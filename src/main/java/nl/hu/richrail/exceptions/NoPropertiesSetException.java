81584236996a9b5fcdd76ac100da2186ff39ee67
joostlek <joostlek@outlook.com>
2018-12-16 14:54:33 +0100

    Remove unused exception

--- src/main/java/nl/hu/richrail/exceptions/NoPropertiesSetException.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.richrail.exceptions;
-
-public class NoPropertiesSetException extends RuntimeException {
-    public NoPropertiesSetException() {
-        super("app.properties or environment variables are empty");
-    }
-}
